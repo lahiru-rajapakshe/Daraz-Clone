6315fcfaa3a572154b74cde0ab681302a6fe4373
lara <lahirurajapakshe.stack@gmail.com>
2022-11-08 22:45:01 +0530

    Implement sample Get method

---
+++ src/main/java/com/example/demo2/controller/frontend/HomeServlet.java
@@ -0,0 +1,26 @@
+package com.example.demo2.controller.frontend;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "/HomeServlet")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String homepage= "forntend/index.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(homepage);
+        dispatcher.forward(request,response);
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
