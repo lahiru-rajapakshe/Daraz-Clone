29f057bd1c63bb22ec980b8e6113977634846c4d
lara <lahirurajapakshe.stack@gmail.com>
2022-11-08 12:34:47 +0530

    Implement UserDAO

--- src/main/java/com/example/demo2/dao/UserDAO.java
+++ src/main/java/com/example/demo2/dao/UserDAO.java
@@ -1,5 +1,39 @@
 package com.example.demo2.dao;
 
-public class UserDAO extends JpaDAO<T> implements GenericDAO<T>{
+import com.example.demo2.entity.UsersEntity;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class UserDAO extends JpaDAO<UsersEntity> implements GenericDAO<UsersEntity>{
+
+
+    public UserDAO(EntityManager entityManager) {
+        super(entityManager);
+    }
+
+    @Override
+    public UsersEntity update(UsersEntity usersEntity) {
+        return null;
+    }
+
+    @Override
+    public UsersEntity get(Object id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Object id) {
+
+    }
+
+    @Override
+    public List<UsersEntity> listAll() {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
 }
