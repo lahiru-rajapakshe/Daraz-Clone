83001cb5b4d9b2f405c7b5d0be67bcec9b014b93
lara <lahirurajapakshe.stack@gmail.com>
2022-11-12 00:12:26 +0530

    Create categoryDAO

---
+++ src/main/java/com/example/demo2/dao/CategoryDAO.java
@@ -0,0 +1,38 @@
+package com.example.demo2.dao;
+
+import com.example.demo2.entity.Category;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class CategoryDAO extends JpaDAO<Category> implements GenericDAO<Category>{
+
+    @Override
+    public Category create(Category category) {
+        return super.create(category);
+    }
+
+    public CategoryDAO(EntityManager entityManager) {
+        super(entityManager);
+    }
+
+    @Override
+    public Category get(Object id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Object id) {
+
+    }
+
+    @Override
+    public List<Category> listAll() {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+}
