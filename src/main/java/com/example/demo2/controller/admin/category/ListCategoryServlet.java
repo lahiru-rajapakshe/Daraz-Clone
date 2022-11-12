1444d6af3d07b66cd64896f364926752d63e5fd4
lara <lahirurajapakshe.stack@gmail.com>
2022-11-12 09:43:56 +0530

    Implement the listCategory method on categoryServlet

--- src/main/java/com/example/demo2/controller/admin/category/ListCategoryServlet.java
+++ src/main/java/com/example/demo2/controller/admin/category/ListCategoryServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class ListCategoryServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-response.getWriter().println("List category");
+        response.getWriter().println("List category");
         CategoryServices categoryServices = new CategoryServices(request, response);
         categoryServices.listCategory();
     }
--- src/main/java/com/example/demo2/service/CategoryServices.java
+++ src/main/java/com/example/demo2/service/CategoryServices.java
@@ -1,12 +1,17 @@
 package com.example.demo2.service;
 
 import com.example.demo2.dao.CategoryDAO;
+import com.example.demo2.entity.Category;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.IOException;
+import java.util.List;
 
 public class CategoryServices {
     private EntityManagerFactory entityManagerFactory;
@@ -26,7 +31,14 @@ public class CategoryServices {
         categoryDAO = new CategoryDAO(entityManager);
 
     }
-public void listCategory(){
+public void listCategory() throws ServletException, IOException {
+    List<Category> listCategories = categoryDAO.listAll();
+    request.setAttribute("listCategory",listCategories);
+
+    String listPage="category_list.jsp";
+    RequestDispatcher requestDispatcher = request.getRequestDispatcher(listPage);
+    requestDispatcher.forward(request,response);
+
 
 }
 
