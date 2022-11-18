8d30ee0bff76b5179b2e29489449c1b18618abb9
lara <lahirurajapakshe.stack@gmail.com>
2022-11-18 10:39:54 +0530

    Refactor the CreateBookServlet class

--- src/main/java/com/example/demo2/controller/admin/book/CreateBookServlet.java
+++ src/main/java/com/example/demo2/controller/admin/book/CreateBookServlet.java
@@ -10,12 +10,12 @@ import java.io.IOException;
 
 @WebServlet(name = "/admin/create_book", value = "/CreateBookServlet")
 @MultipartConfig(fileSizeThreshold = 1024*10,maxFileSize = 1024*300,maxRequestSize = 1024*1024)
-public class CreateBookServlet extends BaseServlet {
+public class CreateBookServlet extends HttpServlet {
 
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        BookServices bookServices = new BookServices(entityManager,request,response);
+        BookServices bookServices = new BookServices(request,response);
         bookServices.createBook();
 
 
