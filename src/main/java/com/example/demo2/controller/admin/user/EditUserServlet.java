29cb17c4641d46a41c934058580525a465b5296e
lara <lahirurajapakshe.stack@gmail.com>
2022-11-18 10:56:39 +0530

    Refactor the EditUserServlet class

--- src/main/java/com/example/demo2/controller/admin/user/EditUserServlet.java
+++ src/main/java/com/example/demo2/controller/admin/user/EditUserServlet.java
@@ -9,10 +9,10 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet(name = "/admin/edit_user", value = "/EditUserServlet")
-public class EditUserServlet extends BaseServlet {
+public class EditUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        UserServices userServices = new UserServices(entityManager,request, response);
+        UserServices userServices = new UserServices(request, response);
         userServices.editUser();
 
     }
