dce01c3fbaba1bb2312f246cf3bf7a044aec28fa
lara <lahirurajapakshe.stack@gmail.com>
2022-11-18 10:42:54 +0530

    Refactor the ListBookServlet class

--- src/main/java/com/example/demo2/controller/admin/book/ListBookServlet.java
+++ src/main/java/com/example/demo2/controller/admin/book/ListBookServlet.java
@@ -9,10 +9,10 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet(name = "/admin/list_books", value = "/ListBookServlet")
-public class ListBookServlet extends BaseServlet {
+public class ListBookServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        BookServices bookServices = new BookServices(entityManager, request, response);
+        BookServices bookServices = new BookServices( request, response);
         bookServices.listBooks();
     }
 
