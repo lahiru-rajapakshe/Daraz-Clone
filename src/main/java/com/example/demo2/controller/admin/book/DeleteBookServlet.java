2225bab867f3999acb1004e325e3960eb4e2b113
lara <lahirurajapakshe.stack@gmail.com>
2022-11-18 10:41:59 +0530

    Refactor the DeleteBookServlet class

--- src/main/java/com/example/demo2/controller/admin/book/DeleteBookServlet.java
+++ src/main/java/com/example/demo2/controller/admin/book/DeleteBookServlet.java
@@ -9,10 +9,10 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet(name = "/admin/delete_book", value = "/DeleteBookServlet")
-public class DeleteBookServlet extends BaseServlet {
+public class DeleteBookServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        BookServices bookServices = new BookServices(entityManager, request, response);
+        BookServices bookServices = new BookServices( request, response);
         bookServices.deleteBook();
     }
 
