1b73638a3c1608fbe05b81fe0349677a84b7c3ae
lara <lahirurajapakshe.stack@gmail.com>
2022-11-18 10:54:53 +0530

    Refactor the EditCategoryServlet class

--- src/main/java/com/example/demo2/controller/admin/category/EditCategoryServlet.java
+++ src/main/java/com/example/demo2/controller/admin/category/EditCategoryServlet.java
@@ -9,10 +9,10 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet(name = "/admin/edit_category", value = "/EditCategoryServlet")
-public class EditCategoryServlet extends BaseServlet {
+public class EditCategoryServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        CategoryServices categoryServices = new CategoryServices(entityManager,request,response);
+        CategoryServices categoryServices = new CategoryServices(request,response);
         categoryServices.editCategory();
 
 
