ff6dbc0c367324594b4030f17c29c030555ef83f
lara <lahirurajapakshe.stack@gmail.com>
2022-11-18 10:55:10 +0530

    Refactor the ListCategoryServlet class

--- src/main/java/com/example/demo2/controller/admin/category/ListCategoryServlet.java
+++ src/main/java/com/example/demo2/controller/admin/category/ListCategoryServlet.java
@@ -9,11 +9,11 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet(name = "/admin/list_category", value = "/ListCategoryServlet")
-public class ListCategoryServlet extends BaseServlet {
+public class ListCategoryServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("List category");
-        CategoryServices categoryServices = new CategoryServices(entityManager,request, response);
+        CategoryServices categoryServices = new CategoryServices(request, response);
         categoryServices.listCategory();
     }
 
