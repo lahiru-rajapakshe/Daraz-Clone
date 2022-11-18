68b1338beb9980e4a55ad783bfd4b33a714c235d
lara <lahirurajapakshe.stack@gmail.com>
2022-11-19 00:04:16 +0530

    Implement CustomerDAO

---
+++ src/main/java/com/example/demo2/dao/CustomerDAO.java
@@ -0,0 +1,35 @@
+package com.example.demo2.dao;
+
+import com.example.demo2.entity.Customer;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+public class CustomerDAO extends JpaDAO<Customer> implements GenericDAO<Customer> {
+    @Override
+    public Customer create(Customer customer) {
+        customer.setRegisterDate((Timestamp) new Date());
+        return super.create(customer);
+    }
+
+    @Override
+    public Customer get(Object id) {
+        return super.find(Customer.class, id);
+    }
+
+    @Override
+    public void delete(Object id) {
+        super.delete(Customer.class, id);
+    }
+
+    @Override
+    public List listAll() {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+}
