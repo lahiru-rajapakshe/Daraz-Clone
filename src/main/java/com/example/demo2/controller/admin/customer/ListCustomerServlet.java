827a9f95c1b58ff0d6d1893ac0ba9b398d6ddd71
lara <lahirurajapakshe.stack@gmail.com>
2022-11-19 10:42:14 +0530

    Finalize listingCustomers

--- src/main/java/com/example/demo2/controller/admin/customer/ListCustomerServlet.java
+++ src/main/java/com/example/demo2/controller/admin/customer/ListCustomerServlet.java
@@ -7,7 +7,7 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "/list_customer", value = "/ListCustomerServlet")
+@WebServlet(name = "/admin/list_customer", value = "/ListCustomerServlet")
 public class ListCustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
