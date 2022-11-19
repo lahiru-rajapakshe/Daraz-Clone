e1b28ba5a93442fc30935a65c7db88ef628b03df
lara <lahirurajapakshe.stack@gmail.com>
2022-11-19 10:26:34 +0530

    Implement the listCustomers on CustomerServices

--- src/main/java/com/example/demo2/service/CustomerServices.java
+++ src/main/java/com/example/demo2/service/CustomerServices.java
@@ -1,7 +1,14 @@
 package com.example.demo2.service;
 
+import com.example.demo2.dao.CustomerDAO;
+import com.example.demo2.entity.Customer;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
 
 public class CustomerServices {
     private HttpServletRequest request;
@@ -13,7 +20,16 @@ public class CustomerServices {
         this.response = response;
     }
 
-    public void listCustomers() {
-        
+    public void listCustomers() throws ServletException, IOException {
+        CustomerDAO customerDAO = new CustomerDAO();
+        List<Customer> listCustomer = customerDAO.listAll();
+
+        request.setAttribute("listCustomer",listCustomer);
+        String listPage="custtomer_list.jsp";
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(listPage);
+        requestDispatcher.forward(request,response);
+
+
+
     }
 }
