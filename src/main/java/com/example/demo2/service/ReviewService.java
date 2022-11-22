ff27d426e54f173582f27b3e89316b5aec9d2d86
lara <lahirurajapakshe.stack@gmail.com>
2022-11-22 13:23:57 +0530

    Implement listAllReview on ReviewService

--- src/main/java/com/example/demo2/service/ReviewService.java
+++ src/main/java/com/example/demo2/service/ReviewService.java
@@ -1,8 +1,14 @@
 package com.example.demo2.service;
 
 import com.example.demo2.dao.ReviewDAO;
+import com.example.demo2.entity.Review;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
 
 public class ReviewService {
     private ReviewDAO reviewDAO;
@@ -17,7 +23,14 @@ public class ReviewService {
 
     }
 
-    public void listAllReview(){
+    public void listAllReview() throws ServletException, IOException {
+        List<Review> reviews =reviewDAO.listAll();
+        request.setAttribute("listReviews",reviews);
+
+        String listPage="review_list.jsp";
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(listPage);
+        requestDispatcher.forward(request,response);
+
 
     }
 }
